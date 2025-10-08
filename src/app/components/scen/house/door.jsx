e2cb6bcd926c989a02732b90bd71db44b014aee7
Arvid <arvid.alund@gmail.com>
2025-10-08 12:25:23 +0200

    nya dörrar om css ändrad

--- src/app/components/scen/house/door.jsx
+++ src/app/components/scen/house/door.jsx
@@ -1,14 +1,13 @@
 export default function Door({ img, exit = false }) {
   return (
     <div
-      className={`absolute bottom-1/7 ${exit ? "exitDoor left-1/10 pixelated" : "left-5/6 normalDoor"}`}
+      className={`absolute bottom-1/7 ${exit ? "exitDoor left-1/10" : "left-5/6 normalDoor"}`}
       style={{
         width: '130px',    
-        height: '200px',      
-        backgroundImage: `url('${img}')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
+        height: '200px',
       }}
-    ></div>
+    >
+      <img src={img} alt="Door" className="w-full h-full pixelated object-cover"/>
+    </div>
   );
 }
