17568298133f1334edee4ffe1dcd1a8cfc3bce1a
Arvid <arvid.alund@gmail.com>
2025-10-13 16:09:50 +0200

    envent listner added

--- src/app/hooks/chests/renderChestProject.jsx
+++ src/app/hooks/chests/renderChestProject.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { onEvent } from "@/app/utils/eventbus";
 
 export default function RenderChestProject({index = 0}){
     const [projects, setProjects] = useState([]);
+    const [show, setShow] = useState(false);
 
     useEffect(()=>{
         const getProjects = async () => {
@@ -18,21 +20,26 @@ export default function RenderChestProject({index = 0}){
         getProjects();
     },[])
 
+    useEffect(()=>{
+        const showProject = onEvent(`showProject-${index}`, ()=>{
+            setShow(true);
+        })
+
+        return () => {
+            showProject?.();
+        }
+    },[])
 
     const project = projects[index];
 
-    return <section className={`bg-[#f7efd8] text-black p-2 rounded w-fit`}>
-        {project ? (
-            <>
-            <h2>{project.name}</h2>
-            <p>{project.description}</p>
-            <div className="flex justify-between underline">
-                <a href={project.github} target="_blank">GitHub</a>
-                <a href={project.url} target="_blank">Demo</a>
-            </div>
-            </>
-        ): (
-            <p>Laddar projektet.....</p>
-        )}
-    </section>
+    return (project && show ? (
+            <section className={`bg-[#f7efd8] text-black p-2 rounded w-fit`}>
+                <h2>{project.name}</h2>
+                <p>{project.description}</p>
+                <div className="flex justify-between underline">
+                    <a href={project.github} target="_blank">GitHub</a>
+                    <a href={project.url} target="_blank">Demo</a>
+                </div>
+            </section>
+        ):null)
 }
