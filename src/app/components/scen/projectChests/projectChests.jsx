da11061879cd4dff12c3d6a4e7bd2fac04c39c5c
Arvid <arvid.alund@gmail.com>
2025-10-13 09:01:24 +0200

    json created, render project function created

--- src/app/components/scen/projectChests/projectChests.jsx
+++ src/app/components/scen/projectChests/projectChests.jsx
@@ -1,10 +1,13 @@
 import RenderChest from "@/app/hooks/chests/useRenderChest";
-
+import RenderChestProject from "@/app/hooks/chests/renderChestProject";
 export default function ProjectChests() {
   return (
     <section className="absolute bottom-1/7 w-full flex justify-evenly">
       {Array.from({ length: 5 }).map((_, index) => (
-        <RenderChest key={index} index={index} />
+        <div>
+          <RenderChestProject index={index} />
+          <RenderChest key={index} index={index} />
+        </div>
       ))}
     </section>
   );
---
+++ src/app/hooks/chests/renderChestProject.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+
+export default function RenderChestProject({index}){
+    return <section>
+        
+    </section>
+}
