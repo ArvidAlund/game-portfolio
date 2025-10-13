be23ec04794c0e25a64d219e60658a7215185172
Arvid <arvid.alund@gmail.com>
2025-10-13 19:29:40 +0200

    resize on click added

--- src/app/components/scen/projectChests/projectChests.jsx
+++ src/app/components/scen/projectChests/projectChests.jsx
@@ -2,15 +2,13 @@ import RenderChest from "@/app/hooks/chests/useRenderChest";
 import RenderChestProject from "@/app/hooks/chests/renderChestProject";
 export default function ProjectChests() {
   return (
-    <section>
-    <section className="absolute bottom-1/7 w-full flex justify-evenly items-end">
+    <section className="fixed bottom-1/7 w-full flex justify-evenly items-end">
       {Array.from({ length: 5 }).map((_, index) => (
         <div key={index} className="flex flex-col items-center justify-center w-[250px]">
           <RenderChestProject index={index} />
           <RenderChest index={index}/>
         </div>
       ))}
-    </section>
     </section>
   );
 }
--- src/app/hooks/chests/renderChestProject.jsx
+++ src/app/hooks/chests/renderChestProject.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect, useRef } from "react";
 import { onEvent } from "@/app/utils/eventbus";
+import useuseExpandAnimation from "./animation/project/useExpandAnimation";
+import { gsap } from "gsap/gsap-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 export default function RenderChestProject({index = 0}){
     const [projects, setProjects] = useState([]);
     const [show, setShow] = useState(false);
+    const [expanded, setExpanded] = useState(false);
     const projectRef = useRef(null);
 
     useEffect(()=>{
@@ -35,21 +40,33 @@ export default function RenderChestProject({index = 0}){
         const element = projectRef.current;
         if (!element) return;
 
-        const handleMouseOver = ()=>{
-            console.log("yes sir")
+        gsap.fromTo(element, {opacity:0, y:50, scale:0}, {opacity:1, y:0, scale:1, duration:0.5, ease:"power1.out"})
+
+        const handleMouseOver = async ()=>{
+            // useuseExpandAnimation(element);
+            element.style = "";
+            setExpanded(true);
         }
 
-        element.addEventListener("mouseover", handleMouseOver);
+        element.addEventListener("click", handleMouseOver);
         return () =>{
-            element.removeListener("mouseover", handleMouseOver);
+            element.removeListener("click", handleMouseOver);
         }
     }, [show])
 
     const project = projects[index];
 
+    const handleCloseClick = ()=>{
+        setExpanded(false)
+    }
+
     return (project && show ? (
-            <section className={`bg-[#f7efd8] text-black p-2 rounded w-fit`} ref={projectRef}>
+            <section className={`bg-[#f7efd8] text-black p-2 rounded cursor-pointer transition-all duration-200 ${expanded ? "fixed w-1/2 left-1/2 top-1/2 -translate-1/2": "static w-fit"}`} ref={projectRef}>
+                {expanded ? (<button className="absolute top-0 right-0" onClick={handleCloseClick}><FontAwesomeIcon icon={faX}/></button>):null}
                 <h2>{project.name}</h2>
+                {expanded ? (
+                    <div className="w-full aspect-video p-1">live demo</div>
+                ): null}
                 <p>{project.description}</p>
                 <div className="flex justify-between underline">
                     <a href={project.github} target="_blank" rel="noopener noreferrer">GitHub</a>
