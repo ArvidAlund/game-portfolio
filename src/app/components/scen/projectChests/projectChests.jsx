6528da7c3e90fe253bb8b8ec20fb78cc71e01324
Arvid <arvid.alund@gmail.com>
2025-10-13 09:51:25 +0200

    get json file and parse data

--- src/app/components/scen/projectChests/projectChests.jsx
+++ src/app/components/scen/projectChests/projectChests.jsx
@@ -4,7 +4,7 @@ export default function ProjectChests() {
   return (
     <section className="absolute bottom-1/7 w-full flex justify-evenly">
       {Array.from({ length: 5 }).map((_, index) => (
-        <div>
+        <div className="bottom-0">
           <RenderChestProject index={index} />
           <RenderChest key={index} index={index} />
         </div>
--- src/app/hooks/chests/renderChestProject.jsx
+++ src/app/hooks/chests/renderChestProject.jsx
@@ -1,7 +1,22 @@
 import { useState, useEffect } from "react";
 
 export default function RenderChestProject({index}){
-    return <section>
+    const [projects, setProjects] = useState([])
+
+    useEffect(()=>{
+        const getProjects = async () => {
+            try{
+                const res = await fetch("/projects.json");
+                const jsonData = await res.json();
+                setProjects(jsonData.projects)
+            } catch (error){
+                console.error("Fel vid hämtning av project: ", error);
+            }
+        }
         
+        getProjects();
+    },[])
+
+    return <section>
     </section>
 }
